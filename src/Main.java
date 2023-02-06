0b95defd29cfe4fd2527e6e98aa587408de4bc4f
yanastyas <yanastyas28@mail.ru>
2023-02-07 00:19:20 +1000

    методы

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,62 @@
+
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
-import static java.lang.Character.toLowerCase;
 
 public class Main {
+
+    public final static String LoremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
     public static void main(String[] args) {
-        String LoremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-        Map<Character, Integer> LoremIpsumMap = new HashMap<>();
-        LoremIpsumMap.put(toLowerCase(LoremIpsum.charAt(0)), 1);
-        for (int i = 1; i < LoremIpsum.length(); i++) {
-            char s = toLowerCase(LoremIpsum.charAt(i));
+        char[] arr = LoremIpsum.toLowerCase(Locale.ROOT).toCharArray();
+        Map<Character, Integer> symbols = new HashMap<>();
+        for (char s : arr) {
             if (Character.isLetter(s)) {
-                if (LoremIpsumMap.containsKey(s)) {
-                    int sAmount = LoremIpsumMap.get(s);
-                    sAmount++;
-                    LoremIpsumMap.put(s, sAmount);
-                } else {
-                    LoremIpsumMap.put(s, 1);
+                Integer integer = symbols.get(s);
+                if (integer == null) {
+                    symbols.put(s, 1);
+                    continue;
                 }
+                symbols.put(s, ++integer);
             }
         }
-        int maxLetter = -1;
-        int minLetter = Integer.MAX_VALUE;
-        char max = 0;
-        char min = 0;
-        for (Character key : LoremIpsumMap.keySet()) {
-            int keyAmount = LoremIpsumMap.get(key);
-            if (keyAmount > maxLetter) {
-                maxLetter = keyAmount;
-                max = key;
-            }
-            if (keyAmount < minLetter) {
-                minLetter = keyAmount;
-                min = key;
+        Integer max = getMax(symbols);
+        Integer min = getMin(symbols);
+
+        System.out.println("В тексте встречается чаще всего буква: ");
+        for (Map.Entry<Character, Integer> entry : symbols.entrySet()) {
+            if (entry.getValue() == max) {
+                System.out.println("'" + entry.getKey() + "'" + " - " + entry.getValue() + " раз(а).");
             }
         }
-        System.out.println("Буква '" + min + "' встречается реже, всего - " + minLetter + " раз(а).");
-        System.out.println("Буква '" + max + "' встречается чаще, всего - " + maxLetter + " раз(а).");
+        System.out.println("В тексте встречается реже всего:");
+        for (Map.Entry<Character, Integer> entry : symbols.entrySet()) {
+            if (entry.getValue() == min) {
+                System.out.println("'" + entry.getKey() + "'" + " - " + entry.getValue() + " раз(а).");
+            }
+        }
     }
+
+    private static Integer getMax(Map<Character, Integer> symbols) {
+        int max = Integer.MIN_VALUE;
+        for (Map.Entry<Character, Integer> entry : symbols.entrySet()) {
+            if (max < entry.getValue()) {
+                max = entry.getValue();
+            }
+        }
+        return max;
+    }
+
+    private static Integer getMin(Map<Character, Integer> symbols) {
+        int min = Integer.MAX_VALUE;
+        for (Map.Entry<Character, Integer> entry : symbols.entrySet()) {
+            if (min > entry.getValue()) {
+                min = entry.getValue();
+            }
+        }
+        return min;
+    }
+
 }
+
